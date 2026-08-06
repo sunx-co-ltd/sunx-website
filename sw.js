f3706683f5121308b6d6cec3dcff5be6918389b3
sunx-co-ltd <sunx-co-ltd@users.noreply.github.com>
2026-08-06 10:18:40 +0000

    deploy: 36672c7f463323486b169154d737b6bf2fa3a6c6

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Service Worker: キャッシュを常に最新に保つ
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
